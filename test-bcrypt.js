d20b93eaf0ff1dc644e0a11444669a11cad53f23
Jared Malan <jared.malan7@gmail.com>
2024-07-24 19:28:23 -0600

    additional testing code

--- test-bcrypt.js
+++ test-bcrypt.js
@@ -1,13 +1,25 @@
+// const bcrypt = require('bcryptjs');
+
+// // The plain text password to compare
+// const password = 'Admin';
+
+// // The new hashed password stored in your database
+// const storedHash = '$2a$10$RTuwmvGHr3OvDtxeD8JynOGZ6me.bUnwzI90nkcD8hqRoBnF8ntHa';
+
+// // Compare the plain text password with the hashed password
+// bcrypt.compare(password, storedHash, (err, result) => {
+//     if (err) throw err;
+//     console.log('Password match:', result); // Should output: true
+// });
+
+
+
 const bcrypt = require('bcryptjs');
 
-// The plain text password to compare
-const password = 'Admin';
+const hashPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+};
 
-// The new hashed password stored in your database
-const storedHash = '$2a$10$RTuwmvGHr3OvDtxeD8JynOGZ6me.bUnwzI90nkcD8hqRoBnF8ntHa';
-
-// Compare the plain text password with the hashed password
-bcrypt.compare(password, storedHash, (err, result) => {
-    if (err) throw err;
-    console.log('Password match:', result); // Should output: true
-});
+hashPassword('Admin');
